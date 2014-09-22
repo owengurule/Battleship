16c39dd3bc200a102dc5e0f797685db8257c30a7
SeedPaths-User <lessdismissive@email.com>
2014-09-22 12:40:09 -0600

    HighScoresUpdate

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -10,11 +10,44 @@ namespace Battleship
     {
         static void Main(string[] args)
         {
+            DisplayHighScores();
+
             //make a new grid
             Grid grid = new Grid();
             //call play game for our grid
             grid.PlayGame();
             
         }
+        static void AddHighScore(int playerScore)
+        {
+            Console.WriteLine("Your name");
+            string playerName = Console.ReadLine();
+            OwenEntities db = new OwenEntities();
+            HighScore newHighScore = new HighScore();
+            newHighScore.DateCreated = DateTime.Now;
+            newHighScore.Game = "Battleship";
+            newHighScore.Name = playerName;
+            newHighScore.Score = playerScore;
+            db.HighScores.Add(newHighScore);
+            db.SaveChanges();
+
+        }
+
+        static void DisplayHighScores()
+        {
+            Console.Clear();
+            Console.WriteLine("BattleShipHighScores");
+            Console.WriteLine("--------------------");
+            OwenEntities db = new OwenEntities();
+            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Battleship").Take(10).ToList();
+
+            foreach (HighScore highScore in highScoreList)
+            {
+                Console.WriteLine("{0}, {1} - {2} on {3}", highScoreList.IndexOf(highScore) + 1, highScore.Name, highScore.Score);
+            }
+
+        }
+
+
     }
 }
